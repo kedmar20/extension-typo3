9f86a055b9d60b8c1df07c3a35e156db3c754f8f
Kedmar20 <m.kedzierski76@gmail.com>
2023-12-12 19:44:16 +0100

    add initial changes

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Typografix1',
+    'description' => 'TYPO 3 Template',
+    'category' => 'plugin',
+    'author' => 'Martin Kedzierski',
+    'author_email' => 'm.kedzierski@typografix.de',
+    'author_company' => 'Typografix GmbH',
+    'state' => 'alpha',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.0.0-12.4.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
